0ac3a8c905ab2ba7b889a962467c821c4604edda
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-05-03 19:33:48 +0200

    Configure the project

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,1 +1,25 @@
-export class Auth {}
+import { Position, Role } from "generated/prisma";
+
+export class AuthEntity {
+  uuid: string;
+  firebaseId: string;
+  email: string;
+  nick: string;
+  city?: string;
+  favoriteClub?: string;
+  favoriteFootballer?: string;
+  role: Role;
+  visitor?: {
+    uuid: string;
+  };
+  team?: {
+    uuid: string;
+    players?: Array<{
+      playerUuid: string;
+      position: Position;
+    }>;
+  };
+  createdAt: Date;
+  updatedAt: Date;
+  bannedAt?: Date;
+}

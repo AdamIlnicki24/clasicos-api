4d7341ba7e8f09001ac4a0d283c3a2a659dc0e7c
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-05-04 15:13:46 +0200

    Improve auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,47 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { Prisma, Role, User } from 'generated/prisma';
+import { PrismaService } from 'prisma/prisma.service';
+import { AuthEntity } from './entities/auth.entity';
+import { RegisterDto } from './dto/register.dto';
+import { EXISTING_USER_EXCEPTION } from 'src/constants/exceptions';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  // TODO: Finish method below
+  // private getSelectFields(): Prisma.UserSelect {}
+
+  async getUserByUid(uid: string): Promise<AuthEntity> {
+    return await this.prismaService.user.findUnique({
+      where: {
+        firebaseId: uid,
+      },
+      // select: this.getSelectFields(),
+    });
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  async createUser(registerDto: RegisterDto): Promise<User> {
+    const user = await this.prismaService.user.findFirst({
+      where: {
+        email: registerDto.email,
+      },
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+    if (user) throw new ConflictException(EXISTING_USER_EXCEPTION);
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    return await this.prismaService.user.create({
+      data: {
+        firebaseId: registerDto.firebaseId,
+        email: registerDto.email,
+        isPrivacyPolicyAccepted: registerDto.isPrivacyPolicyAccepted
+          ? new Date()
+          : null,
+        isTermsAndServicesAccepted: registerDto.isTermsAndServicesAccepted
+          ? new Date()
+          : null,
+        role: Role.Visitor,
+      },
+    });
   }
 }

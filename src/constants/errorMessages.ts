64ebf8cef1232d22ee82a79f277ad4209599665e
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-05-06 17:22:26 +0200

    Add constants

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -1,3 +1,2 @@
-export const TOKEN_VERIFICATION_ERROR_MESSAGE =
-  'Weryfikacja tokena nie powiodła się:';
-export const ACCESS_DENIED_ERROR_MESSAGE = 'Odmowa dostępu.';
+export const TOKEN_VERIFICATION_ERROR_MESSAGE = "Weryfikacja tokena nie powiodła się:";
+export const ACCESS_DENIED_ERROR_MESSAGE = "Odmowa dostępu.";
--- src/constants/exceptions.ts
+++ src/constants/exceptions.ts
@@ -1,2 +1,2 @@
-export const EXISTING_USER_EXCEPTION =
-  'Użytkownik o podanym adresie e-mail już istnieje.';
+export const EXISTING_USER_EXCEPTION = "Użytkownik o podanym adresie e-mail już istnieje.";
+export const PLAYER_NOT_FOUND_EXCEPTION = "Wybrany gracz nie istnieje.";

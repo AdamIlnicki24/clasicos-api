36fc7780c9aee45ea0b1da24c6e5b678450f21f3
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-05-07 22:21:04 +0200

    Correct app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,33 @@
-import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { PlayersModule } from './players/players.module';
-import { AuthService } from './auth/auth.service';
-import { PreauthMiddleware } from './auth/preauth.middleware';
-import { PrismaService } from 'prisma/prisma.service'; // <-- tutaj
-import { CommentsModule } from './comments/comments.module';
+import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import configuration from "./config/configuration";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { AuthModule } from "./auth/auth.module";
+import { PlayersModule } from "./players/players.module";
+import { CommentsModule } from "./comments/comments.module";
+import { PrismaService } from "prisma/prisma.service";
+import { FirebaseService } from "./common/services/firebase.service";
+import { PreauthMiddleware } from "./auth/preauth.middleware";
+import { AuthService } from "./auth/auth.service";
 
 @Module({
-  imports: [UsersModule, AuthModule, PlayersModule, CommentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    UsersModule,
+    AuthModule,
+    PlayersModule,
+    CommentsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, AuthService, PrismaService],
+  providers: [AppService, PrismaService, FirebaseService, AuthService],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(PreauthMiddleware).forRoutes({
-      path: '*',
-      method: RequestMethod.ALL,
-    });
+    consumer.apply(PreauthMiddleware).forRoutes({ path: "*", method: RequestMethod.ALL });
   }
 }

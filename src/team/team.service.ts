534256ea895c32a912f384dd5b5cda7f66eef9f0
AdamIlnicki24 <137420588+AdamIlnicki24@users.noreply.github.com>
2025-05-29 22:47:32 +0200

    Reverse prisma paths

--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, ConflictException, Injectable, NotFoundException } from "@nestjs/common";
 import { Team } from "@prisma/client";
-import { PrismaService } from "../prisma/prisma.service";
+import { PrismaService } from "prisma/prisma.service";
 import { AuthEntity } from "src/auth/entities/auth.entity";
 import { SOMETHING_WENT_WRONG_ERROR_MESSAGE } from "src/constants/errorMessages";
 import { EXISTING_TEAM_EXCEPTION, INVALID_TEAM_EXCEPTION, TEAM_NOT_FOUND_EXCEPTION } from "src/constants/exceptions";

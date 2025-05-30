56bd49c83ea82296ba83db81434d776563e05b97
AdamIlnicki24 <137420588+AdamIlnicki24@users.noreply.github.com>
2025-05-30 13:35:56 +0200

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { ForbiddenException, Injectable, NotFoundException } from "@nestjs/common";
 import { Role, Visitor } from "@prisma/client";
 import { ADMIN_CANNOT_BE_BANNED_EXCEPTION, USER_NOT_FOUND_EXCEPTION } from "../constants/exceptions";
-import { PrismaService } from "src/prisma.service";
+import { PrismaService } from "../prisma.service";
 import { UpdateUserDto } from "./dto/update-user.dto";
 
 @Injectable()

e56143eb51aed2f29c97de4d06060289edd0664b
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-06-08 13:33:15 +0200

    Add roles

--- src/team/team.controller.ts
+++ src/team/team.controller.ts
@@ -1,35 +1,41 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
-import { Team } from "@prisma/client";
+import { Role, Team } from "@prisma/client";
 import { AuthEntity } from "../auth/entities/auth.entity";
 import { User } from "../common/decorators/user.decorator";
 import { CreateTeamDto } from "./dto/create-team.dto";
 import { TeamService } from "./team.service";
 import { UpdateTeamDto } from "./dto/update-team.dto";
+import { Roles } from "../common/decorators/roles.decorator";
 
 @Controller("team")
 export class TeamController {
   constructor(private readonly teamService: TeamService) {}
 
+  @Roles(Role.Admin, Role.Visitor)
   @Post("me")
   async createMyTeam(@Body() createTeamDto: CreateTeamDto, @User() user: AuthEntity): Promise<Team> {
     return await this.teamService.createMyTeam(createTeamDto, user);
   }
 
+  @Roles(Role.Admin, Role.Visitor)
   @Get("me")
   async getMyTeam(@User() user: AuthEntity): Promise<Team> {
     return await this.teamService.getTeamByUuid(user.team?.uuid);
   }
 
+  @Roles(Role.Admin, Role.Visitor)
   @Get(":uuid")
   async getTeamByUuid(@Param("uuid") uuid: string): Promise<Team> {
     return await this.teamService.getTeamByUuid(uuid);
   }
 
+  @Roles(Role.Admin, Role.Visitor)
   @Patch("me")
   async updateMyTeam(@Body() updateTeamDto: UpdateTeamDto, @User() user: AuthEntity): Promise<Team> {
     return await this.teamService.updateMyTeam(updateTeamDto, user);
   }
 
+  @Roles(Role.Admin, Role.Visitor)
   @Delete("me")
   async deleteMyTeam(@User() user: AuthEntity): Promise<Team> {
     return await this.teamService.deleteMyTeam(user);

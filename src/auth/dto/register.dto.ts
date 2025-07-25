822c54dc84b18695da721023c05771ba101794c8
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-07-25 08:28:10 +0200

    Improve password validation

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,16 +1,26 @@
-import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
-import { PASSWORD_MAX_LENGTH_EXCEPTION, PASSWORD_MIN_LENGTH_EXCEPTION } from "../../constants/exceptions";
-import { PASSWORD_MAX_LENGTH, PASSWORD_MIN_LENGTH } from "../../constants/lengths";
+import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength } from "class-validator";
+import { PASSWORD_REGEX } from "../../constants/regex";
+import {
+  EMAIL_MAX_LENGTH_EXCEPTION,
+  PASSWORD_EXCEPTION,
+  PASSWORD_MAX_LENGTH_EXCEPTION,
+} from "../../constants/exceptions";
+import { EMAIL_MAX_LENGTH, PASSWORD_MAX_LENGTH } from "../../constants/lengths";
 
 export class RegisterDto {
   @IsNotEmpty()
   @IsEmail()
-  @MaxLength(127)
+  @MaxLength(EMAIL_MAX_LENGTH, {
+    message: EMAIL_MAX_LENGTH_EXCEPTION,
+  })
   email: string;
 
-  // TODO: Add decorators to validate password
   @IsString()
-  @MinLength(PASSWORD_MIN_LENGTH, { message: PASSWORD_MIN_LENGTH_EXCEPTION })
-  @MaxLength(PASSWORD_MAX_LENGTH, { message: PASSWORD_MAX_LENGTH_EXCEPTION })
+  @Matches(PASSWORD_REGEX, {
+    message: PASSWORD_EXCEPTION,
+  })
+  @MaxLength(PASSWORD_MAX_LENGTH, {
+    message: PASSWORD_MAX_LENGTH_EXCEPTION,
+  })
   password: string;
 }
